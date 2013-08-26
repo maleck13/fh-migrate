a3c3843b5046e85a660ddeef5b016eabf85442a8
craig <craig.brookes@feedhenry.com>
2013-08-26 14:49:27 +0100

    add data adder gettr

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,31 @@
-module.exports = {
-  "nodevers": function (params, callback){
-    return callback(undefined,{"version":process.version});
-  } 
-  
-}
+exports.addData = function (params, callback){
+	$fh.db({
+		"act": "create",
+		"type": "contact",
+		"fields":[{
+			"firstName": "Joe",
+			"lastName": "Bloggs",
+			"address1": "22 Blogger Lane",
+			"address2": "Bloggsville",
+			"country": "Bloggland",
+			"phone": "555-123456"
+		},{
+			"firstName": "JoeB",
+			"lastName": "BloggsY",
+			"address1": "22 Blogger Lane",
+			"address2": "Bloggsville",
+			"country": "Bloggland",
+			"phone": "555-123456"
+		}]
+	}, callback);
+};
+
+
+exports.getData = function (params, callback){
+	$fh.db({
+		"act": "read",
+		"type": "contact",
+		"guid": params["guid"]
+	}, callback);
+};
+
