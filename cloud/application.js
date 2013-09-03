62365588b581d3b117f55cb0d88559edebb49d52
maleck13 <craig.brookes@feedhenry.com>
2013-09-03 19:46:42 +0100

    Update application.js

--- cloud/application.js
+++ cloud/application.js
@@ -1,3 +1,11 @@
-var nodeapp = require("fh-nodeapp");
-nodeapp.HostApp.init();
-nodeapp.HostApp.serveApp(require('main.js'));
+var webapp = require('fh-webapp');
+var express = require('express');
+var mainjs = require('main.js');
+$fh = require('fh-api');
+
+var app = express();
+app.use('/sys', webapp.sys(mainjs));
+app.use('/mbaas', webapp.mbaas);
+app.use('/cloud', webapp.cloud(mainjs));
+
+module.exports = app.listen(process.env.FH_PORT || process.env.VCAP_APP_PORT || 8001);
